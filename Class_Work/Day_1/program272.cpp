8b12c7e2029ccef1d56fa3df850515e157064aee
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-01 08:31:13 +0530

    Add files via upload

---
+++ Class_Work/Day_1/program272.cpp
@@ -0,0 +1,31 @@
+//Generic programming
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+T Summation(T Arr[], int iSize)
+{
+    int iCnt = 0;
+    T iSum;
+
+    for(iCnt = 1, iSum = Arr[0]; iCnt < iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return iSum;
+}
+
+
+int main()
+{
+    int Brr[]  = {10,20,30,40,50};
+    int iRet = 0;
+
+    iRet = Summation(Brr,5);
+
+    cout<<"Summation is : "<<iRet<<"\n";
+
+    return 0;
+}
