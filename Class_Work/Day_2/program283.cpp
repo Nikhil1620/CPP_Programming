dde269d9a1fc3c30b90a4ae6d8ed836a9bcd893a
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-03 11:24:35 +0530

    Add files via upload

---
+++ Class_Work/Day_2/program283.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void Display()
+{
+    int iCnt = 0;
+    while(iCnt != 4)
+    {
+        cout<<"JayGanesh...\n";
+        iCnt++;
+    }
+    
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
