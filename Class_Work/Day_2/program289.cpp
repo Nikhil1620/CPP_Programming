84473617de64023b2a5839cb55e9c2f98d7717db
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-03 11:27:06 +0530

    Add files via upload

---
+++ Class_Work/Day_2/program289.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void auto_Demo()
+{
+    auto i = 1;
+    cout<<"auto_Demo : "<<i<<"\n";
+    i++;
+
+}
+
+void static_Demo()
+{
+    static int i = 1;
+    cout<<"auto_Demo : "<<i<<"\n";
+    i++;
+
+}
+
+int main()
+{
+    auto_Demo();
+    auto_Demo();
+    auto_Demo();
+    auto_Demo();
+
+    cout<<"\n";
+
+    static_Demo();
+    static_Demo();
+    static_Demo();
+    static_Demo();
+
+    return 0;
+}
